098be7d05baf7d48dd3c107201161f7caf0fe72d
Kunduz <talaybekovna.kunduz@gmail.com>
2022-02-23 17:53:47 +0600

    Implement CheckboxWidget.

--- lib/src/widgets/form/widgets/checkbox_widget.dart
+++ lib/src/widgets/form/widgets/checkbox_widget.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import '../models/widget_data.dart';
 
 // TODO: Implement CheckboxWidget
-class CheckboxWidget extends StatelessWidget {
-  const CheckboxWidget({Key? key}) : super(key: key);
+class CheckboxWidget extends StatefulWidget {
+  const CheckboxWidget({Key? key, required this.widgetData}) : super(key: key);
+
+  final WidgetData widgetData;
+
+  @override
+  State<CheckboxWidget> createState() => _CheckboxWidgetState();
+
+}
+
+class _CheckboxWidgetState extends State<CheckboxWidget> {
+  bool value = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CheckboxListTile(
+      autofocus: widget.widgetData.autofocus,  //true
+      title: Text(widget.widgetData.schema['title']),
+      value: value,
+      onChanged: (dynamic val) {
+        setState(() {
+          value = val;
+          widget.widgetData.onChange(context, widget.widgetData.path, val);
+        });
+      },
+    );
   }
 }
