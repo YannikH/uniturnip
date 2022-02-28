8ce0220cc7882354ad47ce7c4eecc036333541d7
Kunduz <talaybekovna.kunduz@gmail.com>
2022-02-28 13:54:22 +0600

    Remove state in CheckboxWidget.

--- lib/src/widgets/form/widgets/checkbox_widget.dart
+++ lib/src/widgets/form/widgets/checkbox_widget.dart
@@ -2,30 +2,21 @@ import 'package:flutter/material.dart';
 import '../models/widget_data.dart';
 
 // TODO: Implement CheckboxWidget
-class CheckboxWidget extends StatefulWidget {
-  const CheckboxWidget({Key? key, required this.widgetData}) : super(key: key);
+class CheckboxWidget extends StatelessWidget {
+  CheckboxWidget({Key? key, required this.widgetData}) : super(key: key);
 
   final WidgetData widgetData;
-
-  @override
-  State<CheckboxWidget> createState() => _CheckboxWidgetState();
-
-}
-
-class _CheckboxWidgetState extends State<CheckboxWidget> {
   bool value = false;
 
   @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
-      autofocus: widget.widgetData.autofocus,  //true
-      title: Text(widget.widgetData.schema['title']),
-      value: value,
+      autofocus: widgetData.autofocus,  //true
+      title: Text(widgetData.schema['title']),
+      value: widgetData.value != value,
       onChanged: (dynamic val) {
-        setState(() {
-          value = val;
-          widget.widgetData.onChange(context, widget.widgetData.path, val);
-        });
+        value = val;
+        widgetData.onChange(context, widgetData.path, val);
       },
     );
   }
