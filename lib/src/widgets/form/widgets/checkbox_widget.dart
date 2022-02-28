31e4c5e4a3a45186ccf439648296dd9047a9579b
Kunduz <talaybekovna.kunduz@gmail.com>
2022-02-28 18:33:54 +0600

    Update CheckboxWidget.

--- lib/src/widgets/form/widgets/checkbox_widget.dart
+++ lib/src/widgets/form/widgets/checkbox_widget.dart
@@ -3,20 +3,18 @@ import '../models/widget_data.dart';
 
 // TODO: Implement CheckboxWidget
 class CheckboxWidget extends StatelessWidget {
-  CheckboxWidget({Key? key, required this.widgetData}) : super(key: key);
+  const CheckboxWidget({Key? key, required this.widgetData}) : super(key: key);
 
   final WidgetData widgetData;
-  bool value = false;
 
   @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
       autofocus: widgetData.autofocus,  //true
       title: Text(widgetData.schema['title']),
-      value: widgetData.value != value,
-      onChanged: (dynamic val) {
-        value = val;
-        widgetData.onChange(context, widgetData.path, val);
+      value: true == widgetData.value,
+      onChanged: (dynamic newValue) {
+        widgetData.onChange(context, widgetData.path, newValue);
       },
     );
   }
