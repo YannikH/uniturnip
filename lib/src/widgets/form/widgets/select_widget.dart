122aac26575ffa2270c9a32b6738b67f15ce7af4
Kunduz <talaybekovna.kunduz@gmail.com>
2022-02-28 10:46:44 +0600

    Implement SelectWidget.

--- lib/src/widgets/form/widgets/select_widget.dart
+++ lib/src/widgets/form/widgets/select_widget.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import '../models/widget_data.dart';
 
-class SelectWidget extends StatelessWidget {
-  const SelectWidget({Key? key}) : super(key: key);
+class SelectWidget extends StatefulWidget {
+  const SelectWidget({Key? key, required this.widgetData}) : super(key: key);
+
+  final WidgetData widgetData;
+
+  @override
+  State<SelectWidget> createState() => _SelectWidgetState();
+}
+
+class _SelectWidgetState extends State<SelectWidget> {
+  late List<String> _options;
+  String selectedChoice = "";
+
+
+  _buildList() {
+    List<Widget> choices = [];
+    _options = widget.widgetData.schema['enum'].cast<String>();
+    for (var item in _options) {
+      choices.add(Container(
+        padding: EdgeInsets.only(top: 10.0, right: 10.0),
+        child: ChoiceChip(
+          label: Text(item),
+          selectedColor: Colors.green,
+          selected: selectedChoice == item,
+          onSelected: (selected) {
+            setState(() {
+              selectedChoice = item;
+              widget.widgetData.onChange(context, widget.widgetData.path, selected);
+            });
+          },
+        ),
+      ));
+    }
+    return choices;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    _options = widget.widgetData.schema['enum'].cast<String>();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(widget.widgetData.schema['title']),
+        Row(
+          children: _buildList(),
+        ),
+      ],
+    );
   }
 }
