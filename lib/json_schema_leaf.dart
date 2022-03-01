2140f4babf2eb243239f9056bd28129fe740c936
aTNa <atay.narynov@gmail.com>
2022-03-01 13:45:10 +0600

    Remove old widget selector

--- lib/json_schema_leaf.dart
+++ lib/json_schema_leaf.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uniturnip/json_schema_field.dart';
+import 'package:uniturnip/src/widgets/form/models/widget_data.dart';
+import 'package:uniturnip/src/widgets/form/utils.dart';
 import 'package:uniturnip/ui_model.dart';
 
 import 'mapPath.dart';
@@ -21,57 +23,15 @@ class JSONSchemaFinalLeaf extends JSONSchemaUIField{
   @override
   Widget build(BuildContext context) {
     print('$path is rebuilding!');
-    // print('Schema: $schema');
-    bool isExternal = false;
     dynamic data = context.select(
             (UIModel uiModel) {
-              isExternal = uiModel.isExternal;
               return uiModel.getDataByPath(path);
             }
     );
-    InputDecoration decoration = InputDecoration(labelText: schema['title']);
-    if (schema.containsKey('enum')) {
-      List<String> options;
-      try {
-        options = schema['enum'].cast<String>();
-      } on NoSuchMethodError catch (error) {
-        return Text(
-            'Error: Enum field of ${schema['title']} field must be a List!');
-      }
-      return DropdownButtonFormField(
-          value: data ?? options[0],
-          items: options
-              .map((e) => DropdownMenuItem(value: e, child: Text(e)))
-              .toList(),
-          decoration: decoration,
-          onChanged: (dynamic val) => onUpdate(context, path, val));
-    } else if ((schema['type'] ?? 'not_defined') == 'boolean') {
-      return CheckboxListTile(
-          title: Text(schema['title']),
-          value: data ?? schema['default'] ?? false,
-          onChanged: (bool? val) => onUpdate(context, path, val));
-    } else {
-      if (isExternal) {
-        textControl.text = data ?? '';
-        textControl.selection = TextSelection.fromPosition(
-          TextPosition(offset: textControl.text.length),
-        );
-        return TextFormField(
-          controller: textControl,
-          // initialValue: data ?? '',
-          onChanged: (val) => onUpdate(context, path, val),
-          decoration: decoration,
-        );
-      } else {
-        return TextFormField(
-          initialValue: data ?? '',
-          onChanged: (val) => onUpdate(context, path, val),
-          decoration: decoration,
-        );
-      }
 
-    }
-    // return const SizedBox.shrink();
+    final WidgetData widgetData =
+    WidgetData(schema: schema, value: data, path: path, onChange: onUpdate);
+    return Utils.formWidget(widgetData);
   }
 
   void onUpdate(BuildContext context, MapPath path, dynamic value) {
