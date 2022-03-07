f0cfa9db196935bf958ebeea46ef470521d7e137
Klooppy <uproad312@gmail.com>
2022-03-07 11:56:25 +0600

    add up_down widget

--- lib/src/widgets/form/widgets/up_down_widget.dart
+++ lib/src/widgets/form/widgets/up_down_widget.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 
 // TODO: Implement UpDownWidget
-class UpDownWidget extends StatelessWidget {
+class UpDownWidget extends StatefulWidget {
   const UpDownWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  State<UpDownWidget> createState() => _UpDownWidgetState();
 }
+
+class _UpDownWidgetState extends State<UpDownWidget> {
+  int page = 1;
+
+  final _controller = PageController();
+  List<Widget> widgets = [ // list of pages I'll be scrolling through
+    Center(child: Text("Page 1", style: TextStyle(fontSize: 50))),
+    Center(child: Text("Page 2", style: TextStyle(fontSize: 50))),
+    Center(child: Text("Page 3", style: TextStyle(fontSize: 50))),
+    Center(child: Text("Page 4", style: TextStyle(fontSize: 50))),
+    Center(child: Text("Page 5", style: TextStyle(fontSize: 50))),
+  ];
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+      body: PageView(
+        children: widgets, // set 'children' to the list of pages
+        scrollDirection: Axis.vertical, // Set the Axis to the desired direction.
+        controller: _controller, // set this to the controller
+        onPageChanged: (num) { // what will happen when you switch
+          setState(() { // Update state
+            page = num; // Set the page we're on to the num argument
+  });
+  }));
+  }
+
+  }
+
+
+
