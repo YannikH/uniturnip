a0bf12878595b2f05aba2f7201aa06e68f6d14d5
aTNa <atay.narynov@gmail.com>
2022-03-17 16:16:35 +0600

    Implement checkboxes_widget.dart

--- lib/json_schema_ui/widgets/checkboxes_widget.dart
+++ lib/json_schema_ui/widgets/checkboxes_widget.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../models/widget_data.dart';
+
 // TODO: Implement CheckboxesWidget
-class CheckboxesWidget extends StatelessWidget {
-  const CheckboxesWidget({Key? key}) : super(key: key);
+class CheckboxesWidget extends StatefulWidget {
+  const CheckboxesWidget({Key? key, required this.widgetData}) : super(key: key);
+
+  final WidgetData widgetData;
 
   @override
+  State<CheckboxesWidget> createState() => _CheckboxesWidgetState();
+}
+
+class _CheckboxesWidgetState extends State<CheckboxesWidget> {
+  List values =  [];
+  
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    List items = widget.widgetData.schema['enum'] ?? [];
+    List value = widget.widgetData.value ?? [];
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) => CheckboxListTile(
+        controlAffinity: ListTileControlAffinity.leading,
+        autofocus: widget.widgetData.autofocus,
+        title: Text(items[index]),
+        value: value.contains(items[index]),
+        onChanged: (dynamic newValue) {
+          setState(() {
+            newValue ? values.add(items[index]) : values.removeWhere((element) => element == items[index]);
+          });
+          widget.widgetData.onChange(context, widget.widgetData.path, values);
+        },
+      ),
+    );
   }
 }
