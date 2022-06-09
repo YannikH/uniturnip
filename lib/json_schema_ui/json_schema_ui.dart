afd9da30a6f2d15d4746ad8c38fec7198a52e000
Kunduz <talaybekovna.kunduz@gmail.com>
2022-06-09 11:40:37 +0600

    Add default field.

--- lib/json_schema_ui/json_schema_ui.dart
+++ lib/json_schema_ui/json_schema_ui.dart
@@ -14,17 +14,19 @@ class JSONSchemaUI extends StatelessWidget {
       UIModel? controller,
       required this.onUpdate})
       : fields = schema['properties']?.keys?.toList() ?? [],
+        dData = {for(var field in schema['properties']?.keys?.toList()) field: data[field] ?? schema['properties'][field]['default']},
         controller = (controller == null)
             ? UIModel()
             : controller,
         super(key: key) {
-    this.controller.data = data;
+    this.controller.data = dData;
     this.controller.onUpdate = onUpdate;
   }
 
   final Map<String, dynamic> schema;
   final Map<String, dynamic> ui;
   final Map<String, dynamic> data;
+  final Map<String, dynamic> dData;
   final List<String> fields;
   final UIModel controller;
   final void Function(
