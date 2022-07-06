2066c2f378493a13a72af50a7d72e4f73dfc8a74
atna <atay.narynov@gmail.com>
2022-07-06 15:38:31 +0600

    Fix showing 'null' in text and number widgets

--- lib/json_schema_ui/widgets/number_widget.dart
+++ lib/json_schema_ui/widgets/number_widget.dart
@@ -15,7 +15,7 @@ class NumberWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     String title = widgetData.schema['title'] ?? '';
     String description = widgetData.schema['description'] ?? '';
-    textControl.text = widgetData.value.toString();
+    textControl.text = widgetData.value ?? '';
     textControl.selection = TextSelection.fromPosition(
       TextPosition(offset: textControl.text.length),
     );
