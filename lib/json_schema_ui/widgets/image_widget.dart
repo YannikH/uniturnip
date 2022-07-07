d3b91048ef98ff89904d7181879e1de22645b20b
Rambo <ramilsalihar@gmail.com>
2022-07-07 14:46:54 +0600

    Updating Image Widget

--- lib/json_schema_ui/widgets/image_widget.dart
+++ lib/json_schema_ui/widgets/image_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:full_screen_image/full_screen_image.dart';
 import 'package:uniturnip/json_schema_ui/widgets/widget_ui.dart';
 import '../../../../json_schema_ui/models/widget_data.dart';
 
@@ -15,27 +14,53 @@ class ImageWidget extends StatelessWidget {
     final List<String> imageList = widgetData.uiSchema['images'];
 
     return WidgetUI(
-      title: title,
-      description: description,
-      child: imageList.length == 1
-          ? FullScreenWidget(child: Image.network(imageList[0]))
-          : FullScreenWidget(
-            child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (ctx, i) => Image.network(imageList[i]),
-                  itemCount: imageList.length,
+        title: title,
+        description: description,
+        child: imageList.length == 1
+            ? GestureDetector(
+                child: Container(
+                  child: Hero(
+                    child: Image.network(imageList[0]),
+                    tag: 'name',
+                  ),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
                 ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              )
+            : SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (ctx, i) => GestureDetector(
+                      child: Container(
+                        child: Image.network(imageList[i]),
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    itemCount: imageList.length,
+                  ),
               ),
-          ),
-            /* Vertical */
-            // FullScreenWidget(
-            //   child: ListView.builder(
-            //     itemBuilder: (ctx, i) => Image.network(imageList[i]),
-            //     itemCount: imageList.length,
-            //   ),
-            // )
-    );
+        /* Vertical */
+        // ListView.builder(
+        //     itemBuilder: (ctx, i) => GestureDetector(
+        //         child: Container(
+        //       child: Image.network(imageList[i]),
+        //       height: MediaQuery.of(context).size.height,
+        //       width: MediaQuery.of(context).size.width,
+        //     ),
+        //       onTap: (){
+        //           Navigation.pop(context);
+        //       },
+        //     ),
+        //     itemCount: imageList.length,
+        //   ),
+        );
   }
 }
